33fc3c80e21e019847da33c8a398dcf44df74ed2
GGR <g.ruschev@gmail.com>
2020-10-22 17:14:51 +0300

    Added react router to project and updated some logic

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import MainLayout from "../../layouts/main/main"
+
+const LoginPage = () => {
+    return (
+        <MainLayout>
+            <div>Login Page</div>
+        </MainLayout>
+    )
+}
+
+export default LoginPage
---
+++ src/pages/publications/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from "../../layouts/main/main"
+import Origamies from '../../components/origamies/Origamies'
+
+const PublicationsPage = () => {
+    return (
+        <MainLayout>
+            <Origamies />
+        </MainLayout>
+    )
+}
+
+export default PublicationsPage
