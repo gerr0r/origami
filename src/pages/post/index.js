f0ca080e4f3da6ac01ae59d2aa3ea4c67a735c75
GGR <g.ruschev@gmail.com>
2020-10-22 21:56:09 +0300

    Add title to pages and image-title to profile page

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import MainLayout from "../../layouts/main/main"
+import Title from "../../components/title/title"
 
 const PostPage = () => {
     return (
         <MainLayout>
+            <Title title="Share your thoughts..." />
             <div>PostPage</div>
         </MainLayout>
     )
