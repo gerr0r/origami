c1dc1c5932088642bf18776f42d553c665206a7c
GGR <g.ruschev@gmail.com>
2020-10-23 12:13:50 +0300

    Changed header element to nav element

--- src/layouts/main/main.js
+++ src/layouts/main/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../../components/header/Header'
+import Nav from '../../components/nav/Nav'
 import Aside from '../../components/aside/Aside'
 import Footer from "../../components/footer/Footer"
 import styles from './main.module.css'
@@ -7,7 +7,7 @@ import styles from './main.module.css'
 const MainLayout = (props) => {
   return (
     <div className={styles.app}>
-      <Header />
+      <Nav />
       <div className={styles.container}>
         <Aside />
         <div className={styles.content}>
