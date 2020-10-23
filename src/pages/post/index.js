a836ef09a2f7b3e37fcfd1a63e8542531298bc7b
GGR <g.ruschev@gmail.com>
2020-10-23 15:29:31 +0300

    Wrapped postpage in div

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import MainLayout from "../../layouts/main/main"
 import Title from "../../components/title/title"
+import styles from "./index.module.css"
 
 const PostPage = () => {
     return (
         <MainLayout>
-            <Title title="Share your thoughts..." />
-            <textarea placeholder="Your thoughts here..." cols="30" rows="10"></textarea>
-            <button>Post</button>
+            <div className={styles.input}>
+                <Title title="Share your thoughts..." />
+                <textarea placeholder="Your thoughts here..." cols="30" rows="10"></textarea>
+                <button>Post</button>
+            </div>
         </MainLayout>
     )
 }
