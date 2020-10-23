d47cea515110834703cdae5cdb843acf262fb5ff
GGR <g.ruschev@gmail.com>
2020-10-23 20:32:06 +0300

    Preliminary update on login and register pages

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,26 @@
 import React from "react"
 import MainLayout from "../../layouts/main/main"
 import Title from "../../components/title/title"
+import SubmitButton from "../../components/buttons/submit"
+import styles from "./index.module.css"
 
 const LoginPage = () => {
     return (
         <MainLayout>
-            <Title title="Login page" />
-            <div>Login Page</div>
+            <div className={styles.forms}>
+                <Title title="Login page" />
+                <div className={styles["form-control"]}>
+                    <label>Email</label>
+                    <input type="email" />
+                </div>
+                <div className={styles["form-control"]}>
+                    <label>Password</label>
+                    <input type="password" />
+                </div>
+                <div className={styles["form-control"]}>
+                    <SubmitButton name="Login" />
+                </div>
+            </div>
         </MainLayout>
     )
 }
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,12 +1,30 @@
 import React from "react"
 import MainLayout from "../../layouts/main/main"
 import Title from "../../components/title/title"
+import SubmitButton from "../../components/buttons/submit"
+import styles from "./index.module.css"
 
 const RegisterPage = () => {
     return (
         <MainLayout>
-            <Title title="Register page" />
-            <div>RegisterPage</div>
+            <div className={styles.forms}>
+                <Title title="Register page" />
+                <div className={styles["form-control"]}>
+                    <label>Email</label>
+                    <input type="email" />
+                </div>
+                <div className={styles["form-control"]}>
+                    <label>Password</label>
+                    <input type="password" />
+                </div>
+                <div className={styles["form-control"]}>
+                    <label>Re-Password</label>
+                    <input type="password" />
+                </div>
+                <div className={styles["form-control"]}>
+                    <SubmitButton name="Register" />
+                </div>
+            </div>
         </MainLayout>
     )
 }
