0c731976eb12c2a51c3c8eb3d171be86f28342a9
gergan ruschev <gergan.ruschev@smartdesign.local>
2020-10-30 12:48:43 +0200

    Migrate register page

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,31 +1,26 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { useHistory } from "react-router-dom"
 import MainLayout from "../../layouts/main/main"
 import Title from "../../components/title/title"
 import FormControl from "../../components/form-control/form-control"
 import Form from "../../components/form/form"
 import AuthContext from "../../authentication/context"
 
-class RegisterPage extends React.Component {
-    constructor(props) {
-        super(props)
+const RegisterPage = () => {
+    const context = useContext(AuthContext)
+    const history = useHistory()
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [rePassword, setRePassword] = useState("")
 
-        this.state = {
-            username: "",
-            password: "",
-            rePassword: ""
-        }
-    }
-
-    static contextType = AuthContext
-
-    checkLogin = e => {
+    const checkLogin = e => {
         e.preventDefault()
 
         fetch("http://localhost:9999/api/user/register", {
             method: "POST",
             body: JSON.stringify({
-                username: this.state.username,
-                password: this.state.password
+                username,
+                password
             }),
             headers: {
                 "Content-Type": "application/json"
@@ -36,41 +31,33 @@ class RegisterPage extends React.Component {
             else if (res.status === 500) throw Error("Something went wrong")
             else {
                 let authToken = res.headers.get("Authorization")
-                document.cookie = `x-auth-token=${authToken}`
+                document.cookie = `x-auth-token=${authToken}; Secure`
                 return res.json()
             }
         })
         .then(data => {
-            this.context.logIn({
+            context.logIn({
                 username: data.username,
                 id: data._id
             })
-            this.props.history.push("/")
+            history.push("/")
         })
         .catch(err => {
             console.log(err.message) //todo
         })
     }
 
-    changeState = e => {
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
-
-    render() {
-        return (
-            <MainLayout>
-                <Form onSubmit={this.checkLogin}>
-                    <Title title="Register page" />
-                    <FormControl element="input" label="Username" id="username" type="text" value={this.state.username} onChange={this.changeState} />
-                    <FormControl element="input" label="Password" id="password" type="password" value={this.state.password} onChange={this.changeState} />
-                    <FormControl element="input" label="Re-Password" id="rePassword" type="password" value={this.state.rePassword} onChange={this.changeState} />
-                    <FormControl element="button" label="Register" />
-                </Form>
-            </MainLayout>
-        )
-    }
+    return (
+        <MainLayout>
+            <Form onSubmit={checkLogin}>
+                <Title title="Register page" />
+                <FormControl element="input" label="Username" id="username" type="text" value={username} onChange={e => setUsername(e.target.value)} />
+                <FormControl element="input" label="Password" id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <FormControl element="input" label="Re-Password" id="rePassword" type="password" value={rePassword} onChange={e => setRePassword(e.target.value)} />
+                <FormControl element="button" label="Register" />
+            </Form>
+        </MainLayout>
+    )
 }
 
 export default RegisterPage
